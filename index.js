d4c1685cd728380eb6de5ed3638213aac43714db
HYR <huangyiran@yolanda.hk>
2025-02-26 11:10:35 +0800

    Updates

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ var ghpages = require("gh-pages");
 ghpages.publish(
   "_book",
   {
-      branch: "main",
-      repo: "https://github.com/YolandaQingniu/HM_QNPLUGIN_DOC.git"
+    branch: "main",
+    repo: "https://github.com/YolandaQingniu/HM_QNPLUGIN_DOC.git"
   },
   function(err) {
     console.log("github更新" + err);
   }
 );
+
+// ghpages.publish(
+//   "_book",
+//   {
+//     branch: "master",
+//     repo: "https://gitee.com/yolandaqingniu/sdk-doc.git"
+//   },
+//   function(err) {
+//     console.log("gitee更新" + err);
+//   }
+// );
