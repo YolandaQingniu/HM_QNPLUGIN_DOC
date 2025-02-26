dac002ad8887bd34deb8e2af69426dcc040e3d38
HYR <huangyiran@yolanda.hk>
2025-02-26 10:16:58 +0800

    Updates

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var ghpages = require("gh-pages");
 ghpages.publish(
   "_book",
   {
-    branch: "main",
-    repo: "https://github.com/YolandaQingniu/HM_QNPLUGIN_DOC.git"
+      branch: "main",
+      repo: "https://github.com/YolandaQingniu/HM_QNPLUGIN_DOC.git"
   },
   function(err) {
     console.log("github更新" + err);
